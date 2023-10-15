c04f981297d0eaaedb664b67195b0dc5603f055f
xing-yv <xing_yv@outlook.com>
2023-10-15 14:12:24 +0800

    将正常和调试模式设为马上写入文件

--- src/fanqie_normal.py
+++ src/fanqie_normal.py
@@ -13,7 +13,7 @@ https://www.gnu.org/licenses/gpl-3.0.html
 本软件提供的是按"原样"提供的，没有任何明示或暗示的保证，包括但不限于适销性和特定用途的适用性。作者不对任何直接或间接损害或其他责任承担任何责任。在适用法律允许的最大范围内，作者明确放弃了所有明示或暗示的担保和条件。
 
 免责声明：
-该程序仅用于学习和研究Python网络爬虫和网页处理技术，不得用于任何非法活动或侵犯他人权益的行为。使用本程序所产生的一切法律责任和风险，均由用户自行承担，与作者和版权持有人无关。作者不对因使用该程序而导致的任何损失或损害承担任何责任。
+该程序仅用于学习和研究Python网络爬虫和网页处理技术，不得用于任何非法活动或侵犯他人权益的行为。使用本程序所产生的一切法律责任和风险，均由用户自行承担，与作者和项目贡献者无关。作者不对因使用该程序而导致的任何损失或损害承担任何责任。
 
 请在使用本程序之前确保遵守相关法律法规和网站的使用政策，如有疑问，请咨询法律顾问。
 
@@ -83,77 +83,7 @@ Gitee:https://gitee.com/xingyv1024/fanqie-novel-download/
             chapter_id_tmp = re.search(r"/(\d+)", chapter_url_tmp).group(1)
             if chapter_id_tmp == start_chapter_id:  # 将 开始索引设置为用户的值
                 start_index = i
-
-    chapter_id = None
-
-    # 遍历每个章节链接
-    for chapter in chapters[start_index:]:
-        time.sleep(0.5)
-        # 获取章节标题
-        chapter_title = chapter.find("a").get_text()
-
-        # 获取章节网址
-        chapter_url = urljoin(url, chapter.find("a")["href"])
-
-        # 获取章节 id
-        chapter_id = re.search(r"/(\d+)", chapter_url).group(1)
-
-        # 构造 api 网址
-        api_url = f"https://novel.snssdk.com/api/novel/book/reader/full/v1/?device_platform=android&parent_enterfrom=novel_channel_search.tab.&aid=2329&platform_id=1&group_id={chapter_id}&item_id={chapter_id}"
-
-        # 尝试获取章节内容
-        chapter_content = None
-        retry_count = 1
-        while retry_count < 4:  # 设置最大重试次数
-            # 获取 api 响应
-            api_response = requests.get(api_url, headers=headers)
-
-            # 解析 api 响应为 json 数据
-            api_data = api_response.json()
-
-            if "data" in api_data and "content" in api_data["data"]:
-                chapter_content = api_data["data"]["content"]
-                break  # 如果成功获取章节内容，跳出重试循环
-            else:
-                if retry_count == 1:
-                    print(f"{chapter_title} 获取失败，正在尝试重试...")
-                print(f"第 ({retry_count}/3) 次重试获取章节内容")
-                retry_count += 1  # 否则重试
-
-        if retry_count == 4:
-            print(f"无法获取章节内容: {chapter_title}，跳过。")
-            continue  # 重试次数过多后，跳过当前章节
-
-        # 提取文章标签中的文本
-        chapter_text = re.search(r"<article>([\s\S]*?)</article>", chapter_content).group(1)
-
-        # 将 <p> 标签替换为换行符
-        chapter_text = re.sub(r"<p>", "\n", chapter_text)
-
-        # 去除其他 html 标签
-        chapter_text = re.sub(r"</?\w+>", "", chapter_text)
-
-        chapter_text = p.fix_publisher(chapter_text)
-
-        # 在小说内容字符串中添加章节标题和内容
-        content += f"\n\n\n{chapter_title}\n{chapter_text}"
-
-        # 打印进度信息
-        print(f"已获取 {chapter_title}")
-
-    # 保存小说更新源文件
-    upd_file_path = os.path.join(data_folder, f"{title}.upd")
-    # 获取当前系统时间
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # 创建要写入元信息文件的内容
-    new_content = f"{current_time}\n{url}\n{chapter_id}\n{encoding}"
-    # 打开文件并完全覆盖内容
-    with open(upd_file_path, "w") as file:
-        file.write(new_content)
-
-    # 根据编码转换小说内容字符串为二进制数据
-    data = content.encode(encoding, errors='ignore')
-
+    file_path = None
     # 根据main.py中用户选择的路径方式，选择自定义路径或者默认
     if path_choice == 1:
         import tkinter as tk
@@ -182,21 +112,84 @@ Gitee:https://gitee.com/xingyv1024/fanqie-novel-download/
                 # 用户取消了对话框，提示重新选择
                 print("您没有选择路径，请重新选择！")
                 continue
-
-            # 用户选择了文件路径，保存文件并退出循环
-            with open(file_path, "wb") as f:
-                f.write(data)
-
-            # 打印完成信息
-            print(f"已保存")
-            break  # 退出循环
+            break
 
     elif path_choice == 0:
         # 定义文件名
-        filename = title + ".txt"
+        file_path = title + ".txt"
 
-        # 保存文件
-        with open(filename, "wb") as f:
+    chapter_id = None
+
+    with open(file_path, "wb") as f:
+        # 遍历每个章节链接
+        for chapter in chapters[start_index:]:
+            time.sleep(0.5)
+            # 获取章节标题
+            chapter_title = chapter.find("a").get_text()
+
+            # 获取章节网址
+            chapter_url = urljoin(url, chapter.find("a")["href"])
+
+            # 获取章节 id
+            chapter_id = re.search(r"/(\d+)", chapter_url).group(1)
+
+            # 构造 api 网址
+            api_url = f"https://novel.snssdk.com/api/novel/book/reader/full/v1/?device_platform=android&parent_enterfrom=novel_channel_search.tab.&aid=2329&platform_id=1&group_id={chapter_id}&item_id={chapter_id}"
+
+            # 尝试获取章节内容
+            chapter_content = None
+            retry_count = 1
+            while retry_count < 4:  # 设置最大重试次数
+                # 获取 api 响应
+                api_response = requests.get(api_url, headers=headers)
+
+                # 解析 api 响应为 json 数据
+                api_data = api_response.json()
+
+                if "data" in api_data and "content" in api_data["data"]:
+                    chapter_content = api_data["data"]["content"]
+                    break  # 如果成功获取章节内容，跳出重试循环
+                else:
+                    if retry_count == 1:
+                        print(f"{chapter_title} 获取失败，正在尝试重试...")
+                    print(f"第 ({retry_count}/3) 次重试获取章节内容")
+                    retry_count += 1  # 否则重试
+
+            if retry_count == 4:
+                print(f"无法获取章节内容: {chapter_title}，跳过。")
+                continue  # 重试次数过多后，跳过当前章节
+
+            # 提取文章标签中的文本
+            chapter_text = re.search(r"<article>([\s\S]*?)</article>", chapter_content).group(1)
+
+            # 将 <p> 标签替换为换行符
+            chapter_text = re.sub(r"<p>", "\n", chapter_text)
+
+            # 去除其他 html 标签
+            chapter_text = re.sub(r"</?\w+>", "", chapter_text)
+
+            chapter_text = p.fix_publisher(chapter_text)
+
+            # 在小说内容字符串中添加章节标题和内容
+            content += f"\n\n\n{chapter_title}\n{chapter_text}"
+
+            # 根据编码转换小说内容字符串为二进制数据
+            data = content.encode(encoding, errors='ignore')
+
             f.write(data)
-        # 打印完成信息
-        print(f"已保存{title}.txt")
+
+            # 打印进度信息
+            print(f"已获取 {chapter_title}")
+
+    # 保存小说更新源文件
+    upd_file_path = os.path.join(data_folder, f"{title}.upd")
+    # 获取当前系统时间
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # 创建要写入元信息文件的内容
+    new_content = f"{current_time}\n{url}\n{chapter_id}\n{encoding}"
+    # 打开文件并完全覆盖内容
+    with open(upd_file_path, "w") as file:
+        file.write(new_content)
+
+    # 打印完成信息
+    print(f"已保存{title}.txt")
