9f52fcae0754d5e8556b5c5af747662a0703ceea
xing-yv <xing_yv@outlook.com>
2023-10-29 16:04:52 +0800

    修改文件内容

--- src/api_edition/fanqie_api.py
+++ src/api_edition/fanqie_api.py
@@ -58,8 +58,7 @@ def fanqie_l(url, encoding):
     intro = soup.find("div", class_="page-abstract-content").get_text()
 
     # 拼接小说内容字符串
-    content = f"""如果需要小说更新，请勿修改文件名
-使用 @星隅(xing-yv) 所作开源工具下载
+    content = f"""使用 @星隅(xing-yv) 所作开源工具下载
 开源仓库地址:https://github.com/xing-yv/fanqie-novel-download
 Gitee:https://gitee.com/xingyv1024/fanqie-novel-download/
 任何人无权限制您访问本工具，如果有向您提供代下载服务者未事先告知您工具的获取方式，请向作者举报:xing_yv@outlook.com
