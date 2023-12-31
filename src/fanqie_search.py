cc28ca4a2a9224eba84c344bc3feb11964aeafd4
hhhhhge <2173376743@qq.com>
2023-12-31 19:14:42 +0800

    modified:   src/fanqie_search.py

--- src/fanqie_search.py
+++ src/fanqie_search.py
@@ -4,8 +4,8 @@ import requests
 # 定义搜索番茄小说的函数
 def fanqie_s(key):
     # 获取搜索结果列表
-    response=requests.get(f'https://fanqienovel.com/api/author/search/search_book/v1?filter=127,127,127,127&page_count=10&page_index=0&query_type=0&query_word={key}').json()
-    book_list=response['data']['search_book_data_list']
+    response = requests.get(f'https://fanqienovel.com/api/author/search/search_book/v1?filter=127,127,127,127&page_count=10&page_index=0&query_type=0&query_word={key}').json()
+    book_list = response['data']['search_book_data_list']
 
     # 打印搜索结果
     for i,j in zip(book_list,range(len(book_list))):
